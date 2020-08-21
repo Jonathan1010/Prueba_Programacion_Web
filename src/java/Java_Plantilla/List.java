4053caa6565444c03d0d84175d092b85722fe00e
Jonathan1010 <51387003+Jonathan1010@users.noreply.github.com>
2020-08-21 14:47:44 -0500

    Tercera Entrega

---
+++ src/java/Java_Plantilla/List.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java_Plantilla;
+
+/**
+ *
+ * @author USUARIO
+ */
+class List<T> {
+
+    void add(Car car) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void remove(Car car) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
